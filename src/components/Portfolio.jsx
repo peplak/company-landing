4f41c5f474439b1d9c91f5827b6809a7fa8a2e96
peplak <62644538+peplak@users.noreply.github.com>
2024-10-14 15:46:37 +0000

    Deployed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,27 +5,8 @@ const portfolioItems = [
   {
     id: 1,
     title: 'Криптология - Курсы Криптовалют',
-    imageUrl: 'https://peplak.github.io/Cryptology-landing/screenshot.png', // Замените на изображение вашего сайта
     description: 'Онлайн-курсы по криптовалютам. Узнайте все о криптоинвестициях и блокчейне.',
     link: 'https://peplak.github.io/Cryptology-landing/', // Ссылка на сайт
-  },
-  {
-    id: 2,
-    title: 'Проект 2',
-    imageUrl: 'https://via.placeholder.com/300',
-    description: 'Описание проекта 2.',
-  },
-  {
-    id: 3,
-    title: 'Проект 3',
-    imageUrl: 'https://via.placeholder.com/300',
-    description: 'Описание проекта 3.',
-  },
-  {
-    id: 4,
-    title: 'Проект 4',
-    imageUrl: 'https://via.placeholder.com/300',
-    description: 'Описание проекта 4.',
   },
 ];
 
